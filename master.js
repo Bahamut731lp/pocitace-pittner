1730b63dd8306adc15cba3a805e5dd6430b793fe
Bahamut731lp <kevin.krpalek@student.sps-cl.cz>
2019-11-22 14:39:59 +0100

    Init

---
+++ master.js
@@ -0,0 +1,4 @@
+let contactButton = document.querySelector("[name='contact']")
+contactButton.addEventListener("click", function() {
+    location = "https://www.facebook.com/Pocitace.Pittner.Servis.PC.a.Notebooku/"
+})
