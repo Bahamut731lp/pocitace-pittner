79e5ab9a027894f3e0788051d25595b6e4ffb3a9
Bahamut731lp <kevin.krpalek@student.sps-cl.cz>
2019-11-22 15:07:20 +0100

    Sekce, Smooth scroll

--- master.js
+++ master.js
@@ -2,3 +2,16 @@ let contactButton = document.querySelector("[name='contact']")
 contactButton.addEventListener("click", function() {
     location = "https://www.facebook.com/Pocitace.Pittner.Servis.PC.a.Notebooku/"
 })
+
+document.querySelectorAll("nav > a").forEach(function(link, index) {
+    link.addEventListener("click", function(event) {
+        event.preventDefault();
+        var selector = this.getAttribute("href");
+        var position = document.querySelector(selector).getBoundingClientRect();
+
+        window.scrollTo({
+          top: position.top,
+          behavior: 'smooth'
+        });
+    })
+});
