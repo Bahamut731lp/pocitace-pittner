5961546801af536dddd83f691e61027f01095da9
Bahamut731lp <heindrich73@gmail.com>
2019-11-24 14:13:01 +0100

    Komentáře

--- master.js
+++ master.js
@@ -1,15 +1,17 @@
+//Vytvoření paralaxu na landing page
 new Rellax('.landing-bg', {
     speed: 2,
     vertical: true,
 });
 
-
+//A to samý na stránce s otevírací dobou
 new Rellax('.opening-background', {
     speed: 2,
     center: false,
     vertical: true
 });
 
+//Když uživatel klikne, přesune ho to postupně dolů místo náhlého skoku
 document.querySelectorAll("nav > a, #contact-btn").forEach(function(link, index) {
     link.addEventListener("click", function(event) {
         event.preventDefault();
