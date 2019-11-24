4550c27c524ec446d1028d6dba033be065ee8ef9
Bahamut731lp <kevin.krpalek@student.sps-cl.cz>
2019-11-24 13:52:05 +0100

    Prototyp hotový

--- master.js
+++ master.js
@@ -15,7 +15,7 @@ contactButton.addEventListener("click", function() {
     //location = "https://www.facebook.com/Pocitace.Pittner.Servis.PC.a.Notebooku/"
 })
 
-document.querySelectorAll("nav > a").forEach(function(link, index) {
+document.querySelectorAll("nav > a, #contact-btn").forEach(function(link, index) {
     link.addEventListener("click", function(event) {
         event.preventDefault();
         var selector = this.getAttribute("href");
